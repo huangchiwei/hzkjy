8834a0cd1927cb78ca36c41be3056d9836078f19
Administrator <Administrator@WIN-20130721CRR>
2015-03-16 22:31:18 +0800

    //

---
+++ Hzkjy/src/com/armysoft/hzkjy/service/member/IncubatedEnterprisesService.java
@@ -0,0 +1,87 @@
+package com.armysoft.hzkjy.service.member;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.armysoft.core.Pagination;
+import org.armysoft.ibatis.dao.BaseDao;
+import org.springframework.stereotype.Service;
+
+import com.armysoft.hzkjy.model.IncubatedEnterprises;
+import com.armysoft.hzkjy.model.MemberBasic;
+/***
+ * 
+ * 
+ * 项目名称：Jpzx
+ * 类名称：MemberBasic
+ * 类描述：
+ * 创建人：Java autoCoder 
+ * 创建时间：
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ * @version 
+ *
+ */
+@Service
+public class IncubatedEnterprisesService extends BaseDao {
+
+	private final String nameSpace = "IncubatedEnterprisesOpt";
+
+	/**
+	 * 条件分页查询题库
+	 * @param params
+	 * @param pager
+	 * @return
+	 */
+	public List<IncubatedEnterprises> getByPage(Map<String, Object> params, Pagination pager) {
+		return super.getPageList(nameSpace, params, pager);
+	}
+
+	/**
+	 * 根据id查询
+	 * @param id
+	 * @return
+	 */
+	public IncubatedEnterprises findByKey(Long id) {
+		return super.nativeSelectOne(nameSpace + ".findById", id);
+	}
+	
+
+	
+	public Integer getCount(Map<String, Object> params){
+		return super.nativeSelectOne(nameSpace + ".sumCount", params);
+	}
+	
+	
+
+	public List  getCyqy(Map<String, Object> params) {
+	return super.nativeList(nameSpace + ".getCyqy", params);
+	}
+
+	/**
+	 * 添加数据
+	 * @param question
+	 */
+	public void insert(IncubatedEnterprises model) {
+		super.defInsert(nameSpace, model);
+	}
+	
+
+	/**
+	 * 修改
+	 * @param question
+	 */
+	public void update(IncubatedEnterprises model) {
+
+		super.defUpdate(nameSpace, model);
+	}
+
+	/**
+	 * 删除
+	 * @param id
+	 */
+	public void delete(Long id) {
+		super.defDelete(nameSpace, id);
+	}
+
+}
