fd748ed0095ed5f3b899045edfa87a1ac7c55039
zhengguoguan <789woshxi!!>
2015-03-29 23:15:57 +0800

    前台

---
+++ Hzkjy/src/com/armysoft/hzkjy/controller/portal/news/NewsController.java
@@ -0,0 +1,117 @@
+package com.armysoft.hzkjy.controller.portal.news;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.armysoft.core.Pagination;
+import org.armysoft.springmvc.controller.BaseController;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import org.armysoft.security.annotation.PermissionsAnno;
+
+import com.armysoft.hzkjy.base.common.Constants;
+import com.armysoft.hzkjy.base.common.WebConstant;
+import com.armysoft.hzkjy.base.util.Cn2Spell;
+import com.armysoft.hzkjy.model.MemberFasic;
+import com.armysoft.hzkjy.model.News;
+import com.armysoft.hzkjy.service.member.NewsService;
+
+
+@Controller("PortalNewsController")
+@RequestMapping("/portal/news")
+public class  NewsController extends BaseController {
+//
+	@Resource
+	private NewsService newsService;
+	@InitBinder   
+    public void initBinder(WebDataBinder binder) {   
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");   
+        dateFormat.setLenient(true);   
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));   
+    }  
+	
+	/**
+	 * 
+	 * @param currentPage
+	 * @param cateCode
+	 * @param model
+	 * @param request
+	 * @return
+	 */
+    @RequestMapping(value = PAGE_LIST)
+	public String list(@PathVariable Integer currentPage,String cateCode,Model model, HttpServletRequest request) {
+		Pagination pager = initPage(currentPage);
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("cateCode", cateCode);
+		model.addAttribute("cateCode", cateCode);
+        model.addAttribute("list", newsService.getByPage(params, pager));
+        model.addAttribute("category", newsService.getCategory(cateCode));
+		model.addAttribute("page", pager);
+		return "/portal/news/list";
+
+	}
+	
+
+	@RequestMapping(value = "/detail/{id}.html")
+	public String detail(@PathVariable("id") Long key,Model model,String cateCode) {
+		model.addAttribute("cateCode", cateCode);
+		model.addAttribute("entity", newsService.findByKey(key));
+		 model.addAttribute("category", newsService.getCategory(cateCode));
+		model.addAttribute("type", "update");
+		return "/portal/news/detail";
+	}
+	
+	 @RequestMapping(value = "/downLoad/{id}.html")
+	  public void downLoad(HttpServletResponse response,HttpServletRequest request,@PathVariable("id") Long id)
+	  {
+		  try
+		    {
+			  Map<String, Object> news=newsService.findByKey(id);
+		      response.setContentType("application/x-download");
+		      String realPath = request.getSession().getServletContext().getRealPath(news.get("filePath").toString());
+		     String fileName=news.get("filePath").toString().substring(news.get("filePath").toString().lastIndexOf("/")+1);
+		     // realPath = URLEncoder.encode(realPath, "UTF-8");
+		      response.addHeader("Content-Disposition", "attachment;filename=" +fileName );
+		      OutputStream out = response.getOutputStream();
+		      InputStream inputStream = new FileInputStream(realPath);
+		      byte[] buffer = new byte[1024];
+		      int i = -1;
+		      while ((i = inputStream.read(buffer)) != -1) {
+		       out.write(buffer, 0, i);
+		       }
+		      out.flush();
+		      out.close();
+		    }
+		    catch (Exception e) {
+		      e.printStackTrace();
+		    }
+
+	    
+	  }
+	
+	
+
+
+}
