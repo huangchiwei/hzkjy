2b7de5af77b72c43bf8987d883506ef20d0eaf45
zhengguoguan <789woshxi!!>
2015-04-18 21:38:13 +0800

    FCK中文字体

--- Hzkjy/WebRoot/js/ckeditor/config.js
+++ Hzkjy/WebRoot/js/ckeditor/config.js
@@ -8,6 +8,7 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	 config.language = 'zh-cn';
 	 config.removePlugins = 'elementspath';
+	 config.font_names='宋体/宋体;黑体/黑体;仿宋/仿宋_GB2312;楷体/楷体_GB2312;隶书/隶书;幼圆/幼圆;微软雅黑/微软雅黑;'+ config.font_names;
 	// config.toolbar = 'Full'; //Basic Full
 	// config.uiColor = '#AADC6E';
 /*	 config.toolbar_Full = [
